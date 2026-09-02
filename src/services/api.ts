68808b8469fdbee062c208167e283a27f1e5e929
Agrim2005 <agrim.816@gmail.com>
2026-09-03 00:18:57 +0530

    Connected frontend to deployed backend in Render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://community-forum-prototype.onrender.com",
+  baseURL: "https://community-forum-prototype.onrender.com/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
